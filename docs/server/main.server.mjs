850f2020e963a7bf118f8f8dbc5c51361be91378
moussa samoil <moussasamoil10@gmail.com>
2024-02-09 03:10:13 +0200

    commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OT4EHRQM.mjs";import"./chunk-G5RTC2P6.mjs";import"./chunk-SLMTYGH3.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-ZCVOTYYP.mjs";import"./chunk-G5RTC2P6.mjs";import"./chunk-SLMTYGH3.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
